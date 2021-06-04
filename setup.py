40eadb9c75c200b9c5312bb14a7ec6a70c5b4c86
GroupingEffects <85290249+GroupingEffects@users.noreply.github.com>
2021-06-04 15:06:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='GRMF',  
+     version='0.1',
+     scripts=['GRMF'] ,
+     author="Anonymous Author",
+     author_email="anonymous@anonymous.com",
+     description="Robust Matrix Factorization with Grouping Effect",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/GroupingEffects/GRMF",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+     ],
+ )
